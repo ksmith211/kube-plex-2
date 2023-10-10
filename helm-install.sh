ca51acddd258788c9c2eb18ff6eaeaa12ee91383
Kevin Smith <excited_heaps0s@icloud.com>
2023-10-10 11:37:26 -0400

    Add config pvc configuration

--- helm-install.sh
+++ helm-install.sh
@@ -4,4 +4,5 @@ helm install plex ./charts/kube-plex \
     --namespace plex \
     --set claimToken=claim-2HZnP_FPUtUnhD4DKsuV \
     --set persistence.data.claimName=media-pvc\
+    --set persistence.config.claimName=config-pvc\
     --set ingress.enabled=false
